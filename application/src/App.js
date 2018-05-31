eb54f9e35ca9b1ffd12f0321df3b858643fbb618
DarkStarPvP <admin@darkstarpvp.com>
2018-05-31 10:59:33 +0200

    made some visual progress

--- application/src/App.js
+++ application/src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
+import { Column, Table, Grid } from 'react-virtualized';
+import data from './Data/data.js';
+import 'react-virtualized/styles.css';
 import './App.css';
+
+const tableStyle = {
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "100%",
+  display: "flex",
+  alignItems: "center"
+}
 
 class App extends Component {
   constructor() {
     super();
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000/').then(res => res.json()).then(data => console.log(data));
-  }
-
   addWin = () => {
+    const player_name = document.getElementById('player_name').value;
+    const kills = document.getElementById('kills').value;
+
+    this.forceUpdate();
+
+    data.push({
+      id: "5",
+      player: player_name,
+      kills: kills,
+      date: new Date()
+    })
+
     fetch('http://localhost:3000/addwin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
-        "player": "New Player",
-        "kills": "9"
+        "player": player_name,
+        "kills": kills
       })
     })
   }
@@ -24,7 +43,43 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.addWin()}
+        <div className="header-container">
+          <input id="player_name" type="text" placeholder="What is your player name?"/>
+          <input id="kills" type="text" placeholder="How many kills did you get?"/>
+          <button onClick={() => this.addWin()}>Add win</button>
+        </div>
+        <div style={tableStyle}>
+          <Table 
+            stlye={{display: "flex"}}
+            width={1903}
+            height={1000}
+            headerHeight={20}
+            rowHeight={30}
+            rowCount={data.length}
+            rowGetter={({ index }) => data[index]}
+            >
+            <Column
+              label='Id'
+              dataKey='id'
+              width={600}
+            />
+            <Column
+              width={600}
+              label='Player'
+              dataKey='player'
+            />
+            <Column
+              width={600}
+              label='Kills'
+              dataKey='kills'
+            />
+            <Column
+              width={1000}
+              label='Date'
+              dataKey='date'
+            />
+          </Table>
+        </div>
       </div>
     );
   }
---
+++ application/src/Data/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        "id": "1",
+        "player": "player1",
+        "kills": 3,
+        "date": new Date()
+    },
+    {
+        "id": "2",
+        "player": "player2",
+        "kills": 8,
+        "date": new Date()
+    },
+    {
+        "id": "3",
+        "player": "player3",
+        "kills": 1,
+        "date": new Date()
+    }
+];
+
+export default data;
